85fd0570967b1a036366d36aeaced13a8ea8586d
Julian Fernandez <jafc@holacons.com>
2021-06-25 10:48:43 +0200

    Initial commit with calculator example.

---
+++ MyFirstAppTests/CalculatorTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyFirstApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp.Tests
+{
+    [TestClass()] // esto se llama custom attributes, test class le indica al compitlador que es una clase de testeo
+    public class CalculatorTests
+    {
+        [TestMethod()] // indica que es un metodo de test, cada uno es una prueba dentro de la clase de prueba
+        public void AddTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Add(2, 2) == 4); //tambien se puede pasar un arreglo de valores
+        }
+
+        [TestMethod()]
+        public void SubstractTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Substract(2, 2) == 0);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Multiply(2, 2) == 4);
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Divide(2, 2) == 1);
+        }
+    }
+}
