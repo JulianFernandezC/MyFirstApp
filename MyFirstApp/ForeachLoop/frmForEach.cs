41e1378e7a25e140a2b572bd023255339fa0d21a
Julian Fernandez <jafc@holacons.com>
2021-06-25 13:48:20 +0200

    Foreach implementation with Arraylist example. Closes #1

---
+++ MyFirstApp/ForeachLoop/frmForEach.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstApp.ForeachLoop
+{
+    public partial class frmForEach : Form
+    {
+        public frmForEach()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var student = new Student();
+            student.StudentId = Convert.ToInt32(txtStudentId.Text);
+            student.Name = txtName.Text;
+            student.Surname = txtSurname.Text;
+            student.Age = Convert.ToInt32(txtAge.Text);
+
+            //siempre y cuando la variable este dentro de un metodo se recomienda hacerlo con var,
+            //para el uso dinamico
+            var studentList = new ArrayList();
+            studentList.Add(student);
+            
+            foreach (Student studentItem in studentList)
+            {
+                MessageBox.Show(studentItem.Name + " " + studentItem.Surname);
+            }
+        }
+    }
+}
