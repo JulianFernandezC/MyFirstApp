4fac97357ce21d25fc31c874ab3a11f049b787a8
Julian Fernandez <jafc@holacons.com>
2021-06-30 09:11:58 +0200

    While loop implemented. Closes #39

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,5 +1,4 @@
-﻿using MyFirstApp.ForeachLoop;
-using MyFirstApp.ForLoop;
+﻿using MyFirstApp.WhileLoop;
 using System;
 using System.Windows.Forms;
 
@@ -15,7 +14,7 @@ namespace MyFirstApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmForLoop());
+            Application.Run(new frmWhile());
         }
     }
 }
---
+++ MyFirstApp/WhileLoop/frmWhile.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace MyFirstApp.WhileLoop
+{
+    partial class frmWhile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtStudentId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(69, 221);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(165, 20);
+            this.txtAge.TabIndex = 14;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(69, 171);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(165, 20);
+            this.txtSurname.TabIndex = 13;
+            // 
+            // txtStudentId
+            // 
+            this.txtStudentId.Location = new System.Drawing.Point(69, 70);
+            this.txtStudentId.Name = "txtStudentId";
+            this.txtStudentId.Size = new System.Drawing.Size(165, 20);
+            this.txtStudentId.TabIndex = 12;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(69, 121);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(165, 20);
+            this.txtName.TabIndex = 11;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(85, 296);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(132, 34);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // frmWhile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 416);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtStudentId);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnSave);
+            this.Name = "frmWhile";
+            this.Text = "frmWhile";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtStudentId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
